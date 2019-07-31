a4319f9e5a8d76cbcddb6db95eed8325805d7b35
khris <khris56@hotmail.com>
2019-07-31 16:18:53 -0500

    list implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 
-/// Modifica el componente para que se puedan agregar tareas
-
 class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
-      </div>
-    )
-  }
+ constructor(props) {
+   super(props);
+   this.state = {
+     tasks: ["Sacar la ropa", "Hacer la cama", "Leer un rato"]
+   }
+   this.handleChange = this.handleChange.bind(this);
+   this.enterHandle = this.enterHandle.bind(this);
+ }
+ handleChange(e) {
+   this.setState({
+     task: e.target.value
+   })
+ }
+ enterHandle(e) {
+   if(e.keyCode === 13){
+     e.preventDefault();
+     this.setState({
+       tasks: this.state.tasks.concat(this.state.task)
+     });
+     this.state.task = "";
+   }
+ }
+ render() {
+   return (
+     <div className="wrapper">
+       <div className="list">
+         <h3>Por hacer:</h3>
+         <ul className="todo">
+          {this.state.tasks.map(task =>
+            <li>{task}</li>
+          )}
+         </ul>
+          <form>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.task} onKeyDown={this.enterHandle} onChange={this.handleChange} />
+          </form>
+       </div>
+     </div>
+   )
+ }
 }
-
 
 export default App;

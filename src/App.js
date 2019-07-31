360e7258f1c22e473e7157b75361c2cdef257974
khris <khris56@hotmail.com>
2019-07-31 16:44:38 -0500

    list implemented

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ class App extends Component {
  constructor(props) {
    super(props);
    this.state = {
-     tasks: ["Sacar la ropa", "Hacer la cama", "Leer un rato"]
+     tasks: ["Sacar la ropa", "Hacer la cama", "Leer un rato"],
+     task : ''
    }
    this.handleChange = this.handleChange.bind(this);
    this.enterHandle = this.enterHandle.bind(this);
  }
  handleChange(e) {
+   console.log("change");
    this.setState({
      task: e.target.value
    })
  }
  enterHandle(e) {
-   if(e.keyCode === 13){
-     e.preventDefault();
-     this.setState({
-       tasks: this.state.tasks.concat(this.state.task)
-     });
-     this.state.task = "";
-   }
+   e.preventDefault();
+   console.log("enter");
+   this.setState({
+    tasks: this.state.tasks.concat(this.state.task)
+   });
+   this.state.task = "";
  }
  render() {
    return (
@@ -29,12 +30,12 @@ class App extends Component {
        <div className="list">
          <h3>Por hacer:</h3>
          <ul className="todo">
-          {this.state.tasks.map(task =>
-            <li>{task}</li>
+          {this.state.tasks.map((task, index) =>
+            <li key={index}>{task}</li>
           )}
          </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.task} onKeyDown={this.enterHandle} onChange={this.handleChange} />
+          <form onSubmit={this.enterHandle}>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.task} onChange={this.handleChange} />
           </form>
        </div>
      </div>
